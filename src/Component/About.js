5180b40a3f97b74bc24eeafac28e31ccc761c495
Shjani49 <shjani49@gmail.com>
2020-07-30 10:28:53 -0600

    Added About component

---
+++ src/Component/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class About extends React.Component{
+
+
+    render()
+    {
+        return(
+            <p>
+                Branches help us work with teams;
+                &nbsp;
+                maintaining the integrity of our
+                &nbsp;
+                commit History!
+            </p>
+        );
+    }
+}
+export default About;
